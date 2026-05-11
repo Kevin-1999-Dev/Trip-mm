f3300a24b25a13565b63395a26250e9025449017
Kaung Zaw Han <koeshwin9999@gmail.com>
2026-05-11 22:23:54 +0630

    build : throw error message and custom hook

--- src/components/TripList/trip.js
+++ src/components/TripList/trip.js
@@ -1,45 +1,44 @@
-import {React,useEffect,useState,useCallback} from 'react'
+import {React,useState} from 'react'
 import "./trip.css";
+import useFetch from '../../hooks/useFetch';
 
 export default function Trip() {
-  let [trips, setTrips] = useState([]);
+ 
   let [url,setUrl] = useState("http://localhost:5000/trips");
 
-  let fetchTrips = useCallback(() => {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => setTrips(data));
-  }, [url]);
-
-  useEffect(() => {
-      fetchTrips();
-  }, [fetchTrips]);
+  let {data:trips, error} = useFetch(url);
 
   return (
-    <div>
-      <div >
-        <button className="filter-btn" onClick={() => setUrl("http://localhost:5000/trips")}>
-          All
-        </button>
-        <button className="filter-btn" onClick={() => setUrl("http://localhost:5000/trips?location=Paris")}>
-          Paris
-        </button>
-        <button className="filter-btn" onClick={() => setUrl("http://localhost:5000/trips?location=Japan")}>
-          Japan
-        </button>
-        <button className="filter-btn" onClick={() => setUrl("http://localhost:5000/trips?location=Myanmar")}>
-          Myanmar
-        </button>
+    <>
+      {error && <p>{error.message}</p>
+      }
+      {!error && 
+        <div>
+        <div >
+          <button className="filter-btn" onClick={() => setUrl("http://localhost:5000/trips")}>
+            All
+          </button>
+          <button className="filter-btn" onClick={() => setUrl("http://localhost:5000/trips?location=Paris")}>
+            Paris
+          </button>
+          <button className="filter-btn" onClick={() => setUrl("http://localhost:5000/trips?location=Japan")}>
+            Japan
+          </button>
+          <button className="filter-btn" onClick={() => setUrl("http://localhost:5000/trips?location=Myanmar")}>
+            Myanmar
+          </button>
+        </div>
+        <ul>
+          {trips && trips.map(trip =>
+            <div style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '10px', boxShadow: '0 2px 4px rgba(0,0,0,0.1)', borderRadius: '4px' }} key={trip.id}>
+              <h4>Name: {trip.name}</h4>
+              <p>Description: {trip.description}</p>
+              <p>Location: {trip.location}</p>
+              <p>Price: ${trip.price.toFixed(2)}</p>
+            </div>)}
+        </ul>
       </div>
-      <ul>
-        {trips.map(trip =>
-          <div style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '10px', boxShadow: '0 2px 4px rgba(0,0,0,0.1)',borderRadius: '4px' }} key={trip.id}>
-          <h4>Name: {trip.name}</h4>
-          <p>Description: {trip.description}</p>
-          <p>Location: {trip.location}</p>
-          <p>Price: ${trip.price.toFixed(2)}</p>
-        </div>)}
-      </ul>
-    </div>
+      }
+    </>
   )
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function useFectch(url)  {
+  let [data, setData] = useState(null);
+  let [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Something went wrong");
+        }
+        return response.json();
+      })
+      .then(data => {
+        setData(data)
+        console.log(data);
+      })
+      .catch(error => {
+        setError(error);
+      });
+      
+  }, [url]);
+
+  return { data, error };
+};
+
